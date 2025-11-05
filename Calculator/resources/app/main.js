159fbb279675a8525a6de23e9f74a1ce5009cb8d
Bhavsar Prathmesh <bhavsarprathmesh71@gmail.com>
2025-11-05 16:14:03 +0530

    Removed big files and unnecessary folders

--- Calculator/resources/app/main.js
+++
@@ -1,6 +0,0 @@
-const { app, BrowserWindow } = require('electron');
-function createWindow() {
-  const win = new BrowserWindow();
-  win.loadFile('index.html');
-}
-app.whenReady().then(createWindow);
